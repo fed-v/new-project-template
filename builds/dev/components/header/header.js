67431e10d21bfb727062ab69a43e89be982bad92
Federico <fed_v@hotmail.com>
2017-11-05 23:40:43 -0500

    Made active links in menu nav

--- builds/dev/components/header/header.js
+++ builds/dev/components/header/header.js
@@ -4,38 +4,48 @@
 
 "use strict";
 
+
 // Load dependencies
 import $ from "jquery";
 import {SITE_NAME} from "../../js/utils/constants.js";  // Don't forget the curly braces!
 
+
 // Import stache template
 import template from "./header.stache";
 
-// Main navigation array
-export const MAIN_NAV = [
-    {
-        title: "link_1",
-        href: "index.html"
-    },
-    {
-        title: "link_2",
-        href: "index2.html"
-    },
-    {
-        title: "link_3",
-        href: "index3.html"
-    },
-    {
-        title: "contact",
-        href: "contact.html"
-    }
-];
 
 // Get current page to set it as active
-let currentUrl = "contact.html";
+let currentUrl =  location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+
+
+// Array of page links. Add new items to the menu nav here!
+let HREF_ARRAY = [
+   "page1",
+   "page2",
+   "page3",
+   "contact"
+];
+
+
+// Array of link objects used in the stache template
+export const MAIN_NAV = [];
+
+
+// Iterate through href array and create an object for each link
+for( let i = 0, length = HREF_ARRAY.length; i <= length; i++) {
+
+    MAIN_NAV.push( {
+        title: HREF_ARRAY[i],
+        href: HREF_ARRAY[i] + ".html",
+        class: (currentUrl === HREF_ARRAY[i] + ".html") ? "active" : "nope"
+    });
+
+}
+
 
 // Append the stache template to the page
-$("#site-header").append(template({ siteName: SITE_NAME, navItems: MAIN_NAV, currentUrl: currentUrl }));
+$("#site-header").append(template({ siteName: SITE_NAME, navItems: MAIN_NAV }));
+
 
 // Hamburger Menu Handler
 $('#navbar-toggle').click(function(){
